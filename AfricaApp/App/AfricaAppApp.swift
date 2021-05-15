a87d508a04e37ee60b6c5466ab60c01b9c965b0c
Aaron Cleveland <aaroncleveland@Aarons-MBP.hsd1.il.comcast.net>
2021-05-14 23:51:43 -0500

    Created launchscreen from info.plist, added assets, and created tabview

---
+++ AfricaApp/App/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Aaron Cleveland on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ AfricaApp/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  AfricaApp
+//
+//  Created by Aaron Cleveland on 5/14/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
